fb8ee792fc24b20a04fba1a7c7794b2b236109ab
diguica <dcontreras@thembsworld.com>
2013-10-11 22:43:22 +0200

    Cambio del color en el menu

--- Resources/app.js
+++ Resources/app.js
@@ -9,39 +9,39 @@ var win1 = Ti.UI.createWindow({
 var table = Ti.UI.createTableView({
     data : [{
         title : 'Action Buttons',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Menu'
     }, {
         title : 'Tabs',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Tabs'
     }, {
         title : 'Other Options',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Options'
     }, {
         title : 'Form',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Form'
     }, {
         title : 'Change Menu',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'MenuItem'
     }, {
         title : 'Different Dialog',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Dialog'
     }, {
         title : 'Multi Line',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Multiline'
     }, {
         title : 'Bubbles',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'Bubbles'
     }, {
         title : 'Scroll View',
-        color: 'orange',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '20dp', fontWeight : 'bold'},
         module : 'ScrollView'
     }]
 });
