e571f4f8f252481a950ec9fe1670cc2c019cb56e
diguica <dcontreras@thembsworld.com>
2013-10-11 23:55:25 +0200

    More Row

--- Resources/app.js
+++ Resources/app.js
@@ -43,18 +43,38 @@ var table = Ti.UI.createTableView({
         title : 'Scroll View',
         color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
         module : 'ScrollView'
-    }, {
-        title : 'Scroll View',
+    },{
+        title : 'Action Buttons',
         color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
-        module : 'ScrollView'
+        module : 'Menu'
     }, {
-        title : 'Scroll View',
+        title : 'Tabs',
         color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
-        module : 'ScrollView'
+        module : 'Tabs'
     }, {
-        title : 'Scroll View',
+        title : 'Other Options',
         color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
-        module : 'ScrollView'
+        module : 'Options'
+    }, {
+        title : 'Form',
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
+        module : 'Form'
+    }, {
+        title : 'Change Menu',
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
+        module : 'MenuItem'
+    }, {
+        title : 'Different Dialog',
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
+        module : 'Dialog'
+    }, {
+        title : 'Multi Line',
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
+        module : 'Multiline'
+    }, {
+        title : 'Bubbles',
+        color: 'green',font: {fontFamily: 'Aller', fontSize: '25dp', fontWeight : 'bold'},
+        module : 'Bubbles'
     }]
 });
 
